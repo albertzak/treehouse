2fbb7f0813a7a47537c0f48ee2b03e7614436c2a
Albert Zak <me@albertzak.com>
2018-03-26 18:25:27 +0200

    Fix plugin path

--- homebridge/start.sh
+++ homebridge/start.sh
@@ -5,4 +5,5 @@ cp /usr/src/app/config.json /data/.homebridge/config.json
 
 DEBUG=* homebridge \
   --debug \
-  --user-storage-path /data/.homebridge \ --plugin-path /usr/src/app/node_modules/
+  --user-storage-path /data/.homebridge \
+  --plugin-path /usr/src/app/node_modules/
