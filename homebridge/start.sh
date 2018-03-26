a5cbd1e56ba5319b4b8bd71abe9ff25621716f9e
Albert Zak <me@albertzak.com>
2018-03-26 19:47:28 +0200

    Start avahi daemon

--- homebridge/start.sh
+++ homebridge/start.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Set host-name and enable-dbus
+sed -i -e "s@#enable-dbus=yes@enable-dbus=yes@" -e "s@#host-name=foo@host-name=$(hostname)@" /etc/avahi/avahi-daemon.conf
+
+echo "--> Stopping avahi-daemon"
+systemctl stop avahi-daemon || true
+
+echo "--> Starting avahi-daemon"
+avahi-daemon --no-drop-root --daemonize
+
+sleep 1s
+
+echo "--> Starting homebridge"
 mkdir -p /data/.homebridge/
 cp /usr/src/app/config.json /data/.homebridge/config.json
 
